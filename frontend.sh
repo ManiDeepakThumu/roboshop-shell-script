041fc2d305b1caa326c670b56139952a9c1abf7d
Mani Deepak T <manideepak0071@gmail.com>
2022-10-04 15:52:47 +0300

    add print statements to frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,22 @@
+ echo installing Nginx
  yum install nginx -y
+
+ echo downloading Nginx Web Content
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
  cd /usr/share/nginx/html
+
+ echo Removing Old Web Content
  rm -rf *
+
+ echo Extracting Web Content
  unzip /tmp/frontend.zip
+
+
  mv frontend-main/static/* .
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+
+ echo Starting Nginx Service
  systemctl enable nginx
  systemctl restart nginx
  
