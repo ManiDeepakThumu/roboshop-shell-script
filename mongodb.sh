c30b6abcc3589bdf554dd4104f32437615ad796c
Mani Deepak T <manideepak0071@gmail.com>
2022-10-06 11:33:40 +0300

    MongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ echo "Installing MongoDB Server"
 yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
 
-echo "MongoDB Listen Address"
+echo "Update MongoDB Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo status = $?
 
