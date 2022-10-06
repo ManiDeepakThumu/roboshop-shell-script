18f76c4432c8f123cd31cb7b95f052a25e25eb5e
Mani Deepak T <manideepak0071@gmail.com>
2022-10-06 17:05:21 +0300

    catalogue functions

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,29 @@
  LOG_FILE=/tmp/frontend
- echo installing Nginx
- yum install nginx -y &>>$LOG_FILE
- echo status = $?
 
- echo downloading Nginx Web Content
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
- echo status = $?
+source common.sh
 
- cd /usr/share/nginx/html
+echo installing Nginx
+yum install nginx -y &>>$LOG_FILE
+StatusCheck $?
 
- echo Removing Old Web Content
- rm -rf * &>>$LOG_FILE
- echo status = $?
+echo downloading Nginx Web Content
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+StatusCheck $?$?
 
- echo Extracting Web Content
- unzip /tmp/frontend.zip &>>$LOG_FILE
- echo status = $?
+cd /usr/share/nginx/html
 
- mv frontend-main/static/* . &>>$LOG_FILE
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+echo Removing Old Web Content
+rm -rf * &>>$LOG_FILE
+StatusCheck $?
 
- echo Starting Nginx Service
- systemctl enable nginx &>>$LOG_FILE
- systemctl restart nginx &>>$LOG_FILE
- echo status = $?
+echo Extracting Web Content
+unzip /tmp/frontend.zip &>>$LOG_FILE
+StatusCheck $?
+
+mv frontend-main/static/* . &>>$LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+
+echo Starting Nginx Service
+systemctl enable nginx &>>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
+echo status = $?
