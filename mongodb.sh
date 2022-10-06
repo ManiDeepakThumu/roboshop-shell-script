61cb3e2b50af6bfe8d46a9d83b481e1ec9a1353f
Mani Deepak T <manideepak0071@gmail.com>
2022-10-06 10:46:28 +0300

    MongoDB

---
+++ mongodb.sh
@@ -0,0 +1,13 @@
+LOG_FILE=/tmp/mongodb
+echo "Setting MongoDB Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+echo status = $?
+
+echo "Installing MongoDB Server"
+yum install -y mongodb-org &>>$LOG_FILE
+echo status = $?
+
+echo "Starting MongoDB Service"
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
+echo status = $?
