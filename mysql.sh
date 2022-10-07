06a839f905f9a03352d022f579f311631dcec97c
Mani Deepak T <manideepak0071@gmail.com>
2022-10-07 17:12:07 +0300

    MySQL

--- mysql.sh
+++ mysql.sh
@@ -37,15 +37,16 @@ if [ $? -eq 0 ]; then
   StatusCheck $?
 fi
 
-# mysql_secure_installation
+echo "Download Schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
+StatusCheck $?
 
-# mysql -uroot -pRoboshop@1
+echo "Extract Schema"
+cd /tmp
+unzip -o mysql.zip &>>$LOG_FILE
+StatusCheck $?
 
-
-
-# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-
-# cd /tmp
-# unzip mysql.zip
-# cd mysql-main
-# mysql -u root -pRoboShop@1 <shipping.sql
+echo "Load Schema"
+cd mysql-main
+mysql -u root -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG_FILE
+StatusCheck $?
