cf517ccc093d1d995c0fbd3a6ef315536367bad4
Mani Deepak T <manideepak0071@gmail.com>
2022-10-08 21:29:15 +0300

    dispatch

--- dispatch.sh
+++ dispatch.sh
@@ -40,7 +40,7 @@ systemctl daemon-reload &>>${LOG_FILE}
 systemctl enable dispatch &>>${LOG_FILE}
 
 echo "Update Systemd Service file"
-SYSTEMD_SETUP &>>${LOG_FILE}
+SYSTEMD_SETUP (${COMPONENT}) &>>${LOG_FILE}
 StatusCheck $?
 
 echo "Start Service"
